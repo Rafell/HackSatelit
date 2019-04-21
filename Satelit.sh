f732e15acd9d96c170f03d2c9de96a0014cf9784
Rafell <45201597+Rafell@users.noreply.github.com>
2019-04-21 17:20:59 +0700

    Update and rename README.md to Satelit.sh

---
+++ Satelit.sh
@@ -0,0 +1,187 @@
+#!/usr/bin/bash
+#tools sederhana
+#prank hack satelit
+#by ./Fell Ganns
+clear
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+
+
+echo " "
+echo  $blue "<|======================================|>"
+echo  $red    "|         Hack Satleit Nasa           |"
+echo  $red    "|                                      |"
+echo  $red    "| Author        : ./Fell Ganns             |"
+echo  $red    "| Contact       : 081316826548         |"
+echo  $red    "| Team Code     : Indonesian Hacker Rulez    |"
+echo  $red    "|    All Member Indonesian Hacker Rulez      |"
+echo  $blue "<|======================================|>"
+sleep 1
+
+echo " "
+echo $green "============== $red MENU $green ================="
+echo $blue "===================================="
+echo $blue "|| $green 1. Geser Satelit           palapa       $blue ||"
+echo $blue "|| $green 2. Geser Satelit Telkom       $blue ||"
+echo $blue "|| $green 3. Geser Satelit Asiasat      $blue ||"
+echo $blue "|| $green 4. Geser Satelit Nasa         $blue ||"
+echo $blue "|| $green 0. Exit The Program           $blue ||"
+echo $blue "===================================="
+echo $blue"╭──[Masukkan Pilihan Anda]>"
+read -p"╰───────./Fell Ganns=•>" gans;
+
+
+
+
+
+if [ $gans = 1 ] || [ $gans = 01 ]
+then
+echo $blue"╭──[Mau Kearah Mana]>"
+read -p"╰───────./Fell Ganns=•>" arah;
+sleep 1
+echo $green"╭──[Mau Digeser Berapa Derajat]>"
+read -p"╰───────./Fell Ganns=•>" derajat;
+sleep 1
+echo $red "Loading"
+sleep 1
+echo $blue "10%"
+sleep 1
+echo $green "20%"
+sleep 1
+echo $cyan "30%"
+sleep 1
+echo $purple "40%"
+sleep 1
+echo $blue "50%"
+sleep 1
+echo $red "60%"
+sleep 1
+echo $green "70%"
+sleep 1
+echo $cyan "80%"
+sleep 1
+echo $purple "90%"
+sleep 1
+echo $blue "100%"
+sleep 1
+echo $green "Prosessing Done"
+sleep 1
+echo  $blue "Satelit Berhasil Digeser ke arah $arah sejauh $derajat derajat"
+fi
+
+if [ $gans = 2 ] || [ $gans = 02 ]
+then
+echo $blue"╭──[Mau Kearah Mana]>"
+read -p"╰───────./Fell Ganns=•>" arah;
+sleep 1
+echo $green"╭──[Mau Digeser Berapa Derajat]>"
+read -p"╰───────./Fell Ganns=•>" derajat;
+sleep 1
+echo $red "Loading"
+sleep 1
+echo $blue "10%"
+sleep 1
+echo $green "20%"
+sleep 1
+echo $cyan "30%"
+sleep 1
+echo $purple "40%"
+sleep 1
+echo $blue "50%"
+sleep 1
+echo $red "60%"
+sleep 1
+echo $green "70%"
+sleep 1
+echo $cyan "80%"
+sleep 1
+echo $purple "90%"
+sleep 1
+echo $blue "100%"
+sleep 1
+echo $green "Prosessing Done"
+sleep 1
+echo  $blue "Satelit Berhasil Digeser ke arah $arah sejauh $derajat derajat"
+fi
+
+if [ $gans = 3 ] || [ $gans = 03 ]
+then
+echo $blue"╭──[Mau Kearah Mana]>"
+read -p"╰───────./Fell Ganns=•>" arah;
+sleep 1
+echo $green"╭──[Mau Digeser Berapa Derajat]>"
+read -p"╰───────./Fell Ganns=•>" derajat;
+sleep 1
+echo $red "Loading"
+sleep 1
+echo $blue "10%"
+sleep 1
+echo $green "20%"
+sleep 1
+echo $cyan "30%"
+sleep 1
+echo $purple "40%"
+sleep 1
+echo $blue "50%"
+sleep 1
+echo $red "60%"
+sleep 1
+echo $green "70%"
+sleep 1
+echo $cyan "80%"
+sleep 1
+echo $purple "90%"
+sleep 1
+echo $blue "100%"
+sleep 1
+echo $green "Prosessing Done"
+sleep 1
+echo  $blue "Satelit Berhasil Digeser ke arah $arah sejauh $derajat derajat"
+fi
+
+if [ $gans = 4 ] || [ $gans = 04 ]
+then
+echo $blue"╭──[Mau Kearah Mana]>"
+read -p"╰───────./Fell Ganns=•>" arah;
+sleep 1
+echo $green"╭──[Mau Digeser Berapa Derajat]>"
+read -p"╰───────./Fell Ganns=•>" derajat;
+sleep 1
+echo $red "Loading"
+sleep 1
+echo $blue "10%"
+sleep 1
+echo $green "20%"
+sleep 1
+echo $cyan "30%"
+sleep 1
+echo $purple "40%"
+sleep 1
+echo $blue "50%"
+sleep 1
+echo $red "60%"
+sleep 1
+echo $green "70%"
+sleep 1
+echo $cyan "80%"
+sleep 1
+echo $purple "90%"
+sleep 1
+echo $blue "100%"
+sleep 1
+echo $green "Prosessing Done"
+sleep 1
+echo  $blue "Satelit Berhasil Digeser ke arah $arah sejauh $derajat derajat"
+fi
+
+if [ $gans = 0 ] || [ $gans = 00 ]
+then
+echo  $red "Bay Bay Hacker"
+echo  $blue "Jangan Lupa Coli Yah"
+echo  $green "https://F3ell17.blogspot.com"
+exit 
+fi
